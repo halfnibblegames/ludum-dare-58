4dc1591da7f43e679be25def87406b912ce01bf7
Tom Rijnbeek <tom@tomrijnbeek.me>
2025-10-04 11:40:19 +0200

    🚧 Add broken mouse input

---
+++ src/Scenes/MemoryBlock.cs
@@ -0,0 +1,31 @@
+using Godot;
+
+namespace HalfNibbleGame.Scenes;
+
+public partial class MemoryBlock : Area2D {
+  public override void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx) {
+    GD.Print(@event);
+
+    if (@event is InputEventMouseButton { Pressed: true, ButtonIndex: MouseButton.Left } me) {
+      GD.Print(me.Position);
+      freeMemory();
+    }
+
+    if (@event is InputEventMouseMotion me2) {
+      GD.Print(me2.Position);
+    }
+  }
+
+  private void freeMemory() {
+    GetNode<ColorRect>("ColorRect").Color = new Color(255, 0, 0);
+  }
+
+  public override void _Ready() {
+    GD.Print("MemoryBlock incoming connections:");
+    foreach (var c in GetIncomingConnections()) {
+      foreach (var cc in c) {
+        GD.Print(cc);
+      }
+    }
+  }
+}
