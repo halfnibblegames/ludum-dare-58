d1a2d2535c59a94d353fd7eda14c3751c5f2624d
Tom Rijnbeek <tom@tomrijnbeek.me>
2025-10-04 13:25:29 +0200

    ⏱️ Add actual time based game loop

---
+++ src/Nodes/ProgressBar.cs
@@ -0,0 +1,16 @@
+using Godot;
+using HalfNibbleGame.Autoload;
+using HalfNibbleGame.Nodes.Systems;
+
+namespace HalfNibbleGame.Nodes;
+
+public partial class ProgressBar : ReferenceRect
+{
+  public override void _Process(double delta) {
+    var gameLoop = Global.Services.Get<GameLoop>();
+    var timeLeft = gameLoop.GarbageCollectingTimeLeft;
+    var ratio = (float) timeLeft / gameLoop.GarbageCollectionDuration;
+    var rect = GetNode<ColorRect>("Progress");
+    rect.Size = new Vector2(ratio * Size.X, Size.Y);
+  }
+}
--- src/Nodes/Systems/GameLoop.cs
+++ src/Nodes/Systems/GameLoop.cs
@@ -6,12 +6,14 @@ namespace HalfNibbleGame.Nodes.Systems;
 
 public sealed partial class GameLoop : Node {
 
-  private static RandomNumberGenerator rng = new();
+  private static readonly RandomNumberGenerator rng = new();
 
-  [Export] private float garbageCollectionDuration = 4f;
+  [Export] public float GarbageCollectionDuration = 4f;
   [Export] private float simulationDuration = 4f;
 
   public bool IsGarbageCollecting { get; private set; }
+  public double GarbageCollectingTimeLeft => garbageCollectTimer?.TimeLeft ?? 0;
+  private SceneTreeTimer? garbageCollectTimer;
 
   public override void _Ready() {
     Global.Services.ProvideInScene(this);
@@ -20,14 +22,21 @@ public sealed partial class GameLoop : Node {
 
   private void startGarbageCollecting() {
     IsGarbageCollecting = true;
-    Animations.Animations.DoDelayed(garbageCollectionDuration, startComputerSimulation);
+    garbageCollectTimer = GetTree().CreateTimer(GarbageCollectionDuration);
+    Animations.Animations.DoDelayed(GarbageCollectionDuration, startComputerSimulation);
   }
 
   private void startComputerSimulation() {
     IsGarbageCollecting = false;
-    Global.Services.Get<MemoryGrid>().AllocateProgram(new ProgramPlaceholder(randomColor()), 6);
-    // Animations.Animations.DoDelayed(SimulationDuration, startGarbageCollecting);
-    startGarbageCollecting();
+    garbageCollectTimer = null;
+
+    var grid = Global.Services.Get<MemoryGrid>();
+    for (var i = 0; i < 3; i++) {
+      Animations.Animations.DoDelayed(
+        rng.RandfRange(0, simulationDuration),
+        () => grid.AllocateProgram(new ProgramPlaceholder(randomColor()), rng.RandiRange(3, 8)));
+    }
+    Animations.Animations.DoDelayed(simulationDuration, startGarbageCollecting);
   }
 
   private record ProgramPlaceholder(Color Color) : IProgram {
