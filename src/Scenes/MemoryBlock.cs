d86da93093a2bf86961bf19aeed0fe0e74b6f720
Tom Rijnbeek <tom@tomrijnbeek.me>
2025-10-04 12:12:26 +0200

    🟥 Allow making square reds

--- src/Scenes/MemoryBlock.cs
+++ src/Scenes/MemoryBlock.cs
@@ -3,29 +3,17 @@ using Godot;
 namespace HalfNibbleGame.Scenes;
 
 public partial class MemoryBlock : Area2D {
-  public override void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx) {
-    GD.Print(@event);
 
-    if (@event is InputEventMouseButton { Pressed: true, ButtonIndex: MouseButton.Left } me) {
-      GD.Print(me.Position);
-      freeMemory();
-    }
-
-    if (@event is InputEventMouseMotion me2) {
-      GD.Print(me2.Position);
+  public override void _Input(InputEvent @event) {
+    // 😭
+    if (@event is InputEventMouseButton { Pressed: true, ButtonIndex: MouseButton.Left } mouseEvent) {
+      if (GetNode<CollisionShape2D>("MouseCollision").Shape.GetRect().HasPoint(mouseEvent.Position - Position)) {
+        freeMemory();
+      }
     }
   }
 
   private void freeMemory() {
     GetNode<ColorRect>("ColorRect").Color = new Color(255, 0, 0);
-  }
-
-  public override void _Ready() {
-    GD.Print("MemoryBlock incoming connections:");
-    foreach (var c in GetIncomingConnections()) {
-      foreach (var cc in c) {
-        GD.Print(cc);
-      }
-    }
   }
 }
