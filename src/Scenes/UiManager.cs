17df338b1c229d7d8f2c2f09a30da28fc1a7c751
Tom Rijnbeek <tom@tomrijnbeek.me>
2025-10-06 22:26:25 +0200

    🔇 Add mute button

--- src/Scenes/UiManager.cs
+++ src/Scenes/UiManager.cs
@@ -9,6 +9,10 @@ public partial class UiManager : Control {
   [Export] private Label? scoreLabel;
   [Export] private Label? scoreNoticeLabel;
   [Export] private Button? defragButton;
+  [Export] private TextureButton? muteButton;
+  [Export] private AudioStreamPlayer? audioPlayer;
+  [Export] private Texture2D? mutedTexture;
+  [Export] private Texture2D? playingTexture;
 
   private int lastKnownScore;
   private double nextScoreUpdate;
@@ -48,5 +52,17 @@ public partial class UiManager : Control {
     // Ideally the game over screen would have been a separate scene but... path of least resistance
     Global.Instance.SwitchScene("uid://veqm5h1saokm");
     // GetNode("/root/Main").Free();
+  }
+
+  public void MuteButtonClicked() {
+    if (audioPlayer is null) return;
+    if (audioPlayer.Playing) {
+      audioPlayer.Stop();
+      muteButton!.TextureNormal = mutedTexture;
+    }
+    else {
+      audioPlayer.Play();
+      muteButton!.TextureNormal = playingTexture;
+    }
   }
 }
